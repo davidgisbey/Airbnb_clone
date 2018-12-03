f5c04c0fa30b74bd133901efae3e889c250399fc
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2018-12-03 16:34:31 +0000

    erreneous test deleted

--- spec/database_connection_spec.rb
+++
@@ -1,9 +0,0 @@
-require './lib/database_connection.rb'
-
-describe Database_connection do
-  it "checks that data from airbnb_test database
-  deletes after an sql query runs" do
-    connect
-
-  end
-end
