86b51a66d5989c2628126a9841bd17e6fdf67bd5
Obsiye <mrabdirizak@gmail.com>
2018-12-03 15:58:34 +0000

    Add properties to Space class that match Space table in database

--- lib/space.rb
+++ lib/space.rb
@@ -1,8 +1,11 @@
 class Space
-	attr_accessor :id
+	attr_accessor :id, :property_name, :price_in_pence, :property_description
 
-	def initialize(id)
+	def initialize(id, property_name, price_in_pence, property_description)
 		@id = id
+		@property_name = property_name
+		@price_in_pence = price_in_pence
+		@property_description = property_description
 	end
 
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,12 +1,23 @@
 require 'space'
 
 describe(Space) do
-
-	let(:space) { Space.new(1) }
+	
+	let(:space) { Space.new(2, 'Buckingham Palace', 245, 'Nice and comfy') }
 	
 	it('should store space id') do
-		space.id = 2
 		expect(space.id).to equal(2)
+	end
+
+	it('should store space property_name') do
+		expect(space.property_name).to eql('Buckingham Palace')
+	end
+
+	it('should store space price_in_pence') do
+		expect(space.price_in_pence).to equal(245)
+	end
+
+	it('should store space property_description') do
+		expect(space.property_description).to eql('Nice and comfy')
 	end
 
 end
