2aa0547cbce3d65f3d2b59f8b8c8583fcf0c3a6b
Obsiye <mrabdirizak@gmail.com>
2018-12-03 15:37:24 +0000

    Add spaces.erb and /spaces route for viewing spaces

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class Airbnb < Sinatra::Base
     'Hello Battle!'
   end
 
+  get '/spaces' do
+    erb(:spaces)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
