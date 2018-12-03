b9c1b34f1482bc0d3176b58b8e14374869af1457
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-12-03 16:57:13 +0000

    Basic form to list a new space. Needs to be connected up.

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class Airbnb < Sinatra::Base
     'Hello Battle!'
   end
 
+  get '/space/add' do
+    erb(:add)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
