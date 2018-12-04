2b9dd5366ed6bad3ae1e7976e5443de291a47e46
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2018-12-04 16:31:35 +0000

    self.find method added. Name added to user class

--- lib/user.rb
+++ lib/user.rb
@@ -3,26 +3,32 @@ require 'bcrypt'
 
 class User
 
-  attr_reader :id ,:username , :email
+  attr_reader :id ,:username , :email, :name
 
-  def initialize(id, email, username)
+  def initialize(id, email, username, name)
     @id = id
     @email = email
     @username = username
+    @name = name
   end
 
-  def self.create(email, username, password, database_connection_class = Database_connection)
+  def self.create(email, username, name, password, database_connection_class = Database_connection)
     enc_password = BCrypt::Password.create(password)
-    result = database_connection_class.sql(("INSERT INTO users (email, username, password) VALUES('#{email}', '#{username}', '#{enc_password}') RETURNING id, email, username;"))
-    User.new(result[0]['id'], result[0]['email'], result[0]['username'])
+    result = database_connection_class.sql(("INSERT INTO users (email, username, name, password)
+      VALUES('#{email}', '#{username}', '#{name}', '#{enc_password}') RETURNING id, email, username, name;"))
+    User.new(result[0]['id'], result[0]['email'], result[0]['username'], result[0]['name'])
   end
 
   def self.email_already_exists?(email_address, database_connection_class = Database_connection)
-    result = database_connection_class.sql("SELECT * FROM users WHERE email = '#{email_address}'")
+    result = database_connection_class.sql("SELECT * FROM users WHERE email = '#{email_address}';")
     result.any?
   end
 
-end
+  def self.find(id)
+    result = Database_connection.sql("SELECT id, email, username, name FROM users WHERE id = #{id}")
+    user = User.new(result[0]['id'], result[0]['email'], result[0]['username'], result[0]['name'])
+    user
+  end
 
 
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,42 +4,46 @@ describe User do
   let(:databaseClassforCreate) {double :databaseClassforCreate, sql: [{"id" => "1", "email" => "test@test.com", "username" => "test"}]}
   let(:databaseClassforEmailEmpty) {double :databaseClassforEmailEmpty, sql: []}
   it("sets id to 1 as passed by user") do
-    user = User.new(1, "test@test.com", "test")
+    user = User.new(1, "test@test.com", "test", "Abdi")
     expect(user.id).to eq 1
   end
 
   it("sets email to nil upon initialization") do
-    user = User.new(1, "test@test.com", "test")
+    user = User.new(1, "test@test.com", "test", "Abdi")
     expect(user.email).to eq "test@test.com"
   end
 
   it("sets username to nil upon initialization") do
-    user = User.new(1, "test@test.com", "test")
+    user = User.new(1, "test@test.com", "test", "Abdi")
     expect(user.username).to eq "test"
   end
 
   it "creates a user with id 1" do
-    user = User.create("test@test.com", "test", "password", databaseClassforCreate)
+    user = User.create("test@test.com", "test", "Abdi", "password", databaseClassforCreate)
     expect(user.id).to eq "1"
   end
 
   it "creates a user with email 'test@test.com'" do
-    user = User.create("test@test.com", "test", "password", databaseClassforCreate)
+    user = User.create("test@test.com", "test", "Slev", "password", databaseClassforCreate)
     expect(user.email).to eq "test@test.com"
   end
 
   it "creates a user with username test" do
-    user = User.create("test@test.com", "test", "password", databaseClassforCreate)
+    user = User.create("test@test.com", "test", "Nim", "password", databaseClassforCreate)
     expect(user.username).to eq "test"
   end
 
   it("returns true when user attempts to register an email that already exists") do
-    user = User.create("test@test.com", "test", "password", databaseClassforCreate)
+    user = User.create("test@test.com", "test", "David", "password", databaseClassforCreate)
     expect(User.email_already_exists?("test@test.com", databaseClassforCreate)).to eq true
   end
 
   it("returns false when user email already exists") do
     expect(User.email_already_exists?("test@test.com", databaseClassforEmailEmpty)).to eq false
+  end
+
+  it "user.find method that finds a user by id" do
+    expect(User.find(1).name).to eq ("David G")
   end
 
 end
