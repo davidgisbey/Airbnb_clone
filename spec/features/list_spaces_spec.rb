074c0405c7b1be8ca091af16fdea9002c8202bea
Obsiye <mrabdirizak@gmail.com>
2018-12-04 14:59:30 +0000

    Update test indentation format

--- spec/features/list_spaces_spec.rb
+++ spec/features/list_spaces_spec.rb
@@ -1,19 +1,26 @@
 feature 'List Spaces' do
 
-    scenario 'return space property_name' do
-        visit('/spaces')
-        expect(page).to have_content "David's house"
-        expect(page).to have_content "The Stockport viaduct"
-    end
-    scenario 'Display the price on /spaces' do
-        visit('/spaces')
-        expect(page).to have_content "£100.00"
-        expect(page).to have_content "£0.20"
-    end
-    scenario 'display the description' do
-        visit('/spaces')
-        expect(page).to have_content "This is a house where david lives most of the time"
-        expect(page).to have_content "stay for a night underneath the stockport"
-    end
+  scenario 'return space property_name' do
+    visit '/spaces'
+    expect(page).to have_content "David's house"
+    expect(page).to have_content "The Stockport viaduct"
+	end
+	
+  scenario 'Display the price on /spaces' do
+    visit '/spaces'
+    expect(page).to have_content "£100.00"
+    expect(page).to have_content "£0.20"
+	end
+	
+  scenario 'display the description' do
+    visit '/spaces'
+    expect(page).to have_content "This is a house where david lives most of the time"
+    expect(page).to have_content "stay for a night underneath the stockport"
+	end
+	
+  scenario 'display an email button' do
+		visit '/spaces'
+    expect(page).to have_selector(:link_or_button, 'contact owner')
+  end
 
 end
